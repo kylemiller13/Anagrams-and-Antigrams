5a51104293a8e7b570ed9a632413035551bb70e7
Kyle Miller <millerkyle85@gmail.com>
2022-04-23 21:43:02 -0700

    remove punctuation with regex

--- lib/anagrams.rb
+++ lib/anagrams.rb
@@ -3,12 +3,14 @@ require('pry')
 class Anagram
   attr_accessor(:word1, :word2)
   def initialize(word1, word2)
-    @word1 = word1.downcase().delete(" ")
-    @word2 = word2.downcase().delete(" ")
+    # @word1 = word1.downcase().delete(" ")
+    # @word2 = word2.downcase().delete(" ")
+    @word1 = word1.delete(' ').downcase.gsub(/[!@#$%^&*()-=_+|;':",.<>?']/, '').split(//).sort
+    @word2 = word2.delete(' ').downcase.gsub(/[!@#$%^&*()-=_+|;':",.<>?']/, '').split(//).sort
   end
   def vowel_checker()
     vowels = ["a","e","i","o","u","y"]
-    split_arry = @word1.split('')
+    split_arry = @word1
     split_arry.each do |letter|
       if vowels.include?(letter)
         return true
@@ -18,8 +20,8 @@ class Anagram
   end
 
   def antigram_checker()
-    phrase1 = @word1.split('')
-    phrase2 = @word2.split('')
+    phrase1 = @word1
+    phrase2 = @word2
     phrase1.each do |letter|
       if phrase2.include?(letter)
         return true
@@ -29,18 +31,17 @@ class Anagram
   end
 
   def is_anagram()
-    word1_arry = @word1.split('').sort
-    word2_arry = @word2.split('').sort
+    word1_arry = @word1
+    word2_arry = @word2
     if antigram_checker() == false
       return "These words have no letter matches and are antigrams"
     elsif vowel_checker() == false
-          return "You need to input actual words!"
+      return "You need to input actual words!"
     elsif word1_arry == word2_arry
-          return "is an anagram"
-        else
-          return "is NOT an anagram"
+      return "is an anagram"
+    else
+      return "is NOT an anagram"
     end
   end
 end
-anagram = Anagram.new("ruby","bury")
-p anagram.is_anagram
+
