141862ca4159b61113ad45c7f6f12d96984d9078
Kyle Miller <millerkyle85@gmail.com>
2022-04-23 17:51:34 -0700

    test pass 4

--- spec/anagrams_spec.rb
+++ spec/anagrams_spec.rb
@@ -9,16 +9,20 @@ describe(Anagram) do
       expect(anagram.is_anagram).to(eq("is an anagram"))
     end
   end
-  it('will check to see that two words are anagrams if they have capital letters') do
+  it('will check to see that two words are anagrams no matter the casing') do
     anagram = Anagram.new("Tea","Eat")
     expect(anagram.is_anagram).to(eq("is an anagram"))
   end
   it('will check to see that two words contain vowels') do
     anagram = Anagram.new("pkmn", "kmpn")
-    expect(anagram.is_anagram).to(eq("is NOT an anagram"))
+    expect(anagram.is_anagram).to(eq("You need to input actual words!"))
   end
-  it("will check to see spaces shouldn't count") do 
-    anagram = Anagram.new("r u b y ", "b u r y")
+  it('will check wheather they are actually antigrams') do
+    anagram = Anagram.new("hi","bye")
+    expect(anagram.is_anagram).to(eq("These words have no letter matches and are antigrams"))
+  end
+  it("will check to see spaces & punctuation shouldn't count") do 
+    anagram = Anagram.new("The Morse Code", "Here come dots!")
     expect(anagram.is_anagram).to(eq("is an anagram"))
   end
 end
